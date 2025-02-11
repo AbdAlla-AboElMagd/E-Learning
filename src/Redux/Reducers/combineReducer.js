778679d1be5d2c56a8c415b9865ffecbed1e9050
AbdAlla-AboElMagd <abd.moh1990@gmail.com>
2025-02-11 20:04:37 +0200

    Adding Favorites Reducer

--- src/Redux/Reducers/combineReducer.js
+++ src/Redux/Reducers/combineReducer.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
+import favReducer from "./FavReducer";
 
 export default combineReducers({
   auth: authReducer,
+  favCourses: favReducer,
 });
