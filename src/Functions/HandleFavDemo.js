c06287b5f84fd7b248a1b15e6f2a008f0bee0564
AbdAlla-AboElMagd <abd.moh1990@gmail.com>
2025-02-11 20:22:32 +0200

    Adding Handle Function Demo

---
+++ src/Functions/HandleFavDemo.js
@@ -0,0 +1,24 @@
+// import { useDispatch, useSelector } from "react-redux";
+
+// import { AddFav, DelFav } from "../Redux/Actions/ChangeFav";
+
+// let favCourses = useSelector((state) => state.favCourses.favCourses);
+// let total_fav = useSelector((state) => state.favCourses.totalFav);
+// const dispatch = useDispatch();
+// function CourseObj(id, title, course_img, course_desc) {
+//   return {
+//     id: id,
+//     title: title,
+//     course_img: course_img,
+//     course_desc: course_desc,
+//   };
+// }
+// const handleFav = (id, title, course_img, course_desc) => {
+//   let myPayload = {};
+//   myPayload.id = id;
+//   myPayload.data = CourseObj(id, title, course_img, course_desc);
+
+//   favCourses[id] == undefined
+//     ? dispatch(AddFav(myPayload))
+//     : dispatch(DelFav(myPayload));
+// };
