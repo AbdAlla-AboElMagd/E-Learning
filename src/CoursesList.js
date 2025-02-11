aa76be987caf83f3e9bebdf578ad1f644814ee46
AbdAlla-AboElMagd <abd.moh1990@gmail.com>
2025-02-11 22:43:59 +0200

    Fixing Favorite Courses Page

--- src/CoursesList.js
+++ src/CoursesList.js
@@ -6,52 +6,59 @@ import { Container, Box, Typography, Pagination } from "@mui/material";
 import { Favorite, FavoriteBorder } from "@mui/icons-material";
 import { useSelector } from "react-redux";
 
-
 function CoursesList() {
-    let total_fav = useSelector((state) => state.favCourses.totalFav);
-    const API_URL = "https://retoolapi.dev/3apaeZ/data";
-    const [courses, setCourses] = useState([]);
-    const [page, setPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(1);
-    const limit = 10;
-    const navigate = useHistory();
+  let total_fav = useSelector((state) => state.favCourses.totalFav);
+  const API_URL = "https://retoolapi.dev/3apaeZ/data";
+  const [courses, setCourses] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const limit = 10;
+  const navigate = useHistory();
 
-    useEffect(() => {
-        fetchCourses();
-    }, [page]);
+  useEffect(() => {
+    fetchCourses();
+  }, [page]);
 
-    const fetchCourses = async () => {
-        try {
-            const response = await axios.get(`${API_URL}?_page=${page}&_limit=${limit}`);
-            setCourses(response.data);
-            setTotalPages(Math.ceil(response.headers["x-total-count"] / limit));
-        } catch (error) {
-            console.error("Error fetching courses:", error);
-        }
-    };
+  const fetchCourses = async () => {
+    try {
+      const response = await axios.get(
+        `${API_URL}?_page=${page}&_limit=${limit}`
+      );
+      setCourses(response.data);
+      setTotalPages(Math.ceil(response.headers["x-total-count"] / limit));
+    } catch (error) {
+      console.error("Error fetching courses:", error);
+    }
+  };
 
-    return (
-        <Container>
-            <Link to="/E-Learning/FavCourses">
-<Favorite />   {" "}          
-   <span> {total_fav} </span>
-            </Link>
-            <Typography variant="h4" align="center" gutterBottom>Courses</Typography>
-            <Box display="flex" flexWrap="wrap" justifyContent="center" gap={3}>
-                {courses.map((course) => (
-                    <CourseCard key={course.id} course={course} />
-                ))}
-            </Box>
-            <Pagination
-                count={totalPages}
-                page={page}
-                onChange={(e, value) => setPage(value)}
-                color="primary"
-                style={{ marginTop: 20, display: "flex", justifyContent: "center" }}
-            />
-        </Container>
-    );
-};
+  return (
+    <Container>
+      <Typography variant="h4" align="center" gutterBottom>
+        Courses
+      </Typography>
+      <Typography variant="h5" align="center" gutterBottom>
+        <Link
+          to="/E-Learning/FavCourses"
+          style={{ textDecoration: "none", color: "red" }}
+        >
+          <Favorite /> <span> {total_fav} </span>
+        </Link>
+      </Typography>
 
+      <Box display="flex" flexWrap="wrap" justifyContent="center" gap={3}>
+        {courses.map((course) => (
+          <CourseCard key={course.id} course={course} />
+        ))}
+      </Box>
+      <Pagination
+        count={totalPages}
+        page={page}
+        onChange={(e, value) => setPage(value)}
+        color="primary"
+        style={{ marginTop: 20, display: "flex", justifyContent: "center" }}
+      />
+    </Container>
+  );
+}
 
 export default CoursesList;
--- src/Pages/GetAllFavoriteCourses.js
+++ src/Pages/GetAllFavoriteCourses.js
@@ -1,24 +1,33 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import CourseCard from "../CourseCard";
 import { Box, Container, Typography } from "@mui/material";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { Favorite } from "@mui/icons-material";
 
 function GetAllFavoriteCourses(props) {
   let favCourses = useSelector((state) => state.favCourses.favCourses);
-  let totalFav = useSelector((state) => state.favCourses.totalFav);
-  console.log("hghjhj")
-  console.log(favCourses)
+  let total_fav = useSelector((state) => state.favCourses.totalFav);
+
+  console.log(favCourses);
+
   return (
     <Container>
-      <Typography variant="h4" align="center" gutterBottom>Favorite Coursses</Typography>
-      <Typography variant="h5" align="center" gutterBottom>Total Results: {totalFav}</Typography>
+      <Typography variant="h4" align="center" gutterBottom>
+        Favorite Courses
+      </Typography>
+      <Typography variant="h5" align="center" gutterBottom>
+        <Link
+          to="/E-Learning/FavCourses"
+          style={{ textDecoration: "none", color: "red" }}
+        >
+          <Favorite /> <span> {total_fav} </span>
+        </Link>
+      </Typography>
       <Box display="flex" flexWrap="wrap" justifyContent="center" gap={3}>
-      {Object.entries(favCourses).map(([id, course]) => {
-        <>
-        <div> hjf{id} </div>
-            <CourseCard key={course.id} course={course} />
-          </>
-          })}
-          </Box>
+        {Object.entries(favCourses).map(([id, course]) => (
+          <CourseCard key={course.id} course={course} />
+        ))}
+      </Box>
     </Container>
   );
 }
